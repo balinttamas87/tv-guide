7a9261e95553cd06cf7f666ddc00f4fa2703b896
Tamas Balint <balint.tamas87@gmail.com>
2020-10-25 23:15:57 +0000

    Render 1 schedule with calculated length

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import TVGuide from './features/TVGuide/TVGuide';
+import React, { useEffect, useState } from "react";
+import TVGuide from "./features/TVGuide/TVGuide";
 // import './App.css';
 
 function App() {
+	// const initialState: InitialState = {};
+	const [services, setServices] = useState([]);
+	const [schedule, setSchedule] = useState({ events: [] });
 
-  // const initialState: InitialState = {};
-  const [services, setServices] = useState([])
+	useEffect(() => {
+		const servicesUrl =
+			"https://cdn.skyq.sky.com/recruitment/tvguide/services.json";
+		const scheduleUrl =
+			"https://cdn.skyq.sky.com/recruitment/tvguide/schedule/20200129/2002.json";
 
-  useEffect(() => {
-    const servicesUrl = "https://cdn.skyq.sky.com/recruitment/tvguide/services.json";
-    const scheduleUrl = "https://cdn.skyq.sky.com/recruitment/tvguide/schedule/20200129/2002.json";
+		fetch(servicesUrl)
+			.then((res) => res.json())
+			.then((services) => {
+				console.log({ services });
+				setServices(services);
+			});
 
-    fetch(servicesUrl)
-      .then((res) => res.json())
-      .then(services => {
-        console.log({ services });
-        setServices(services)
-      });
+		fetch(scheduleUrl)
+			.then((res) => res.json())
+			.then((schedule) => {
+				console.log({ schedule });
+				setSchedule(schedule);
+			});
+	}, []);
 
-    fetch(scheduleUrl)
-      .then((res) => res.json())
-      .then(schedule => console.log({ schedule }));
-  }, [])
-
-  return (
-    <div className="App">
-      <TVGuide services={services} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<TVGuide services={services} schedule={schedule} />
+		</div>
+	);
 }
 
 export default App;
--- src/features/TVGuide/TVGuide.tsx
+++ src/features/TVGuide/TVGuide.tsx
@@ -1,20 +1,46 @@
-import React from 'react';
-import ServiceBox from './components/ServiceBox/ServiceBox';
+import React from "react";
+import ServiceBox from "./components/ServiceBox/ServiceBox";
 
-function TVGuide({ services }: any) {
-    return (
-        <div>
-            {services.map((service: any) => (
-                <ServiceBox 
-                    number={service.c}
-                    format={service.sf}
-                    genre={service.sg}
-                    id={service.sid}
-                    title={service.t}
-                />
-            ))}
-        </div>
-    );
+function TVGuide({ services, schedule }: any) {
+	const getWidth = (duration: number) => {
+		const widthOfOneHourBlock = 280; // 280px
+		return `${(duration / 3600) * widthOfOneHourBlock}px`;
+	};
+
+	return (
+		<div style={{ display: "flex", flexDirection: "row" }}>
+			<div>
+				{services.map((service: any) => (
+					<ServiceBox
+						key={service.sid}
+						number={service.c}
+						format={service.sf}
+						genre={service.sg}
+						id={service.sid}
+						title={service.t}
+					/>
+				))}
+			</div>
+			{/* { border: "1px solid black" } */}
+			<div style={{ display: "flex" }}>
+				{schedule.events.map((event: any) => (
+					<div
+						key={event.eid}
+						style={{
+							display: "block",
+							border: "1px solid black",
+							lineHeight: "67px",
+							width: getWidth(event.d),
+							height: "67px",
+							overflow: "hidden",
+						}}
+					>
+						{event.t}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default TVGuide;
