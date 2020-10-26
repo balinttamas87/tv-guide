361a342d11a15e1956dee4a489ac577fa345b871
Tamas Balint <balint.tamas87@gmail.com>
2020-10-26 13:58:02 +0000

    Set same height for ServiceBox and ServiceProgram

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,30 @@
 import React, { useEffect, useState } from "react";
 import TVGuide from "./features/TVGuide/TVGuide";
-// import './App.css';
 
 function App() {
-	// const initialState: InitialState = {};
 	const [services, setServices] = useState([]);
 	const [schedules, setSchedules] = useState<any>([]);
 
 	useEffect(() => {
 		const servicesUrl =
 			"https://cdn.skyq.sky.com/recruitment/tvguide/services.json";
-		// const scheduleUrl =
-		// 	"https://cdn.skyq.sky.com/recruitment/tvguide/schedule/20200129/2002.json";
 
 		fetch(servicesUrl)
 			.then((res) => res.json())
 			.then((services) => {
-				console.log({ services });
-				setServices(services);
+				setServices(services.slice(0, 20));
 				Promise.all(
-					services.map((service: any) =>
-						fetch(
-							`https://cdn.skyq.sky.com/recruitment/tvguide/schedule/20200129/${service.sid}.json`
-						).then((res: any) => res.json())
-					)
+					services
+						.slice(0, 20)
+						.map((service: any) =>
+							fetch(
+								`https://cdn.skyq.sky.com/recruitment/tvguide/schedule/20200129/${service.sid}.json`
+							).then((res: any) => res.json())
+						)
 				).then((schedules) => {
-					console.log({ schedules });
 					setSchedules(schedules);
 				});
 			});
-
-		// fetch(scheduleUrl)
-		// 	.then((res) => res.json())
-		// 	.then((schedule) => {
-		// 		console.log({ schedule });
-		// 		setSchedule(schedule);
-		// 	});
 	}, []);
 
 	return (
