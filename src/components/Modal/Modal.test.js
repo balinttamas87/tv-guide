0a32ca4da694062f96fde2f77a297d3297840a6e
Tamas Balint <balint.tamas87@gmail.com>
2020-10-30 00:10:26 +0000

    Add some simple tests for <Modal />

---
+++ src/components/Modal/Modal.test.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Modal from "./Modal";
+
+
+describe("Test pagination component", () => {
+	it("should render Modal with the body content provided as a component", () => {
+		const ModalBodyContent = () => (<div>content of modal body</div>);
+		const { queryByText } = render(
+			<Modal
+				isOpen={true}
+				onCloseModal={() => { }}
+				modalBodyContent={<ModalBodyContent />}
+			/>
+		);
+
+		expect(queryByText(/content of modal body/i)).not.toBeNull();
+
+	});
+
+	it("should not render anything", () => {
+		const ModalBodyContent = () => (<div>content of modal body</div>);
+
+		const { queryByText } = render(
+			<Modal
+				isOpen={false}
+				onCloseModal={() => { }}
+				modalBodyContent={<ModalBodyContent />}
+			/>
+		);
+
+		expect(queryByText(/content of modal body/i)).toBeNull();
+	});
+
+	it("should call onCloseModal", () => {
+		const onCloseModal = jest.fn();
+		const ModalBodyContent = () => (<div>content of modal body</div>);
+
+		const { queryByText } = render(
+			<Modal
+				isOpen={true}
+				onCloseModal={onCloseModal}
+				modalBodyContent={<ModalBodyContent />}
+			/>
+		);
+
+		const closeButton = queryByText(/Close/i).closest('button');
+		fireEvent.click(closeButton)
+
+		expect(onCloseModal).toHaveBeenCalled();
+	});
+
+});
